16cfd5b2993b8a6ff4d1093696b55ea9a4613dfc
Matheus Leandro <mattleandroo@outlook.com>
2020-08-24 14:26:55 -0300

    implementation of functionalities

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,10 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const valueFormated = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+
+  return valueFormated;
+};
 
 export default formatValue;
